379b64397e543cc3ee9aab5332880979cec52ecd
Hajeekn <77675898+ladjeek-actions@users.noreply.github.com>
2022-04-04 10:03:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ const index = async () => {
     date: Date.now() + ''
   }
   const pageageFile = {
-    name: 'wytong-pub',
+    name: process.env.NPM_NAME || '',
     version: `${obj.date[0]}.${obj.date[1]}.${obj.date.slice(2, 13)}`,
     main: 'index.js',
-    author: 'Hajeekn',
-    repository: {"type": "git","url": "git+https://github.com/x-ga/hubnpm.git"},
     description: 'Hajeekn的自动化储存'
   }
   const pageageFileStr = JSON.stringify(pageageFile)
